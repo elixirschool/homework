d75c681117ff4156b379b80de6bb29535b08af5d
Sean Callan <seancallan@gmail.com>
2018-04-29 13:23:27 -0600

    Add markdown lesson

---
+++ markdown/lib/markdown.ex
@@ -0,0 +1,11 @@
+defmodule Markdown do
+  @moduledoc """
+  A simple Markdown parser.
+  """
+
+  @doc """
+  Parse a string of Markdown into HTML
+  """
+  def parse(markdown, opts \\ []) do
+  end
+end
