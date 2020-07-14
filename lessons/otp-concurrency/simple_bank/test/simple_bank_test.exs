59f3af77ca99658551391ee108d8df0fbabeba7d
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2020-07-14 08:04:30 -0400

    update simple bank tests

--- lessons/otp-concurrency/simple_bank/test/simple_bank_test.exs
+++ lessons/otp-concurrency/simple_bank/test/simple_bank_test.exs
@@ -9,7 +9,7 @@ defmodule SimpleBankTest do
   end
 
   describe "register/2" do
-    test "creates a new account and generates an account id", %{bank: bank_pid} do
+    test "creates a new account with the given name and a randomly generated id. HINT: We've included the `elixir-uuid` dependency so that you can generate a random UUID string. See docs here https://github.com/zyro/elixir-uuid", %{bank: bank_pid} do
       {:ok, account_id} = SimpleBank.register(bank_pid, "Another Test Account")
       assert is_binary(account_id)
     end
@@ -21,7 +21,7 @@ defmodule SimpleBankTest do
 
   describe "deposit/3" do
     test "increases the account balance by the deposited amount", %{bank: bank_pid}  do
-      assert {:ok, 10} == SimpleBank.deposit(bank_pid, "test_id", 10)
+      assert {:ok, 110} == SimpleBank.deposit(bank_pid, "test_id", 10)
     end
 
     test "does not allow deposits of negative amounts", %{bank: bank_pid}  do
@@ -35,7 +35,7 @@ defmodule SimpleBankTest do
 
   describe "balance/2" do
     test "returns the current account balance", %{bank: bank_pid}  do
-      assert {:ok, 110} == SimpleBank.deposit(bank_pid, "test_id", 10)
+      assert {:ok, 100} == SimpleBank.balance(bank_pid, "test_id")
     end
 
     test "raises an error if the account does not exist", %{bank: bank_pid}  do
@@ -45,15 +45,15 @@ defmodule SimpleBankTest do
 
   describe "withdrawl/3" do
     test "decreases the account balance by the withdrawn amount", %{bank: bank_pid}  do
-      assert {:ok, 100} == SimpleBank.withdrawl(bank_pid, "test_id", 10)
+      assert {:ok, 90} == SimpleBank.withdrawl(bank_pid, "test_id", 10)
     end
 
     test "does not negative ammount balances", %{bank: bank_pid}  do
-      assert {:error, :insufficient_funds} == SimpleBank.withdrawl(bank_pid, "test_id", -1)
+      assert {:error, :insufficient_funds} == SimpleBank.withdrawl(bank_pid, "test_id", 1000)
     end
 
     test "does not allow withdrawls of negative amounts", %{bank: bank_pid}  do
-      assert {:error, :pos_integer_only} == SimpleBank.withdrawl(bank_pid, "test_id", 1000)
+      assert {:error, :pos_integer_only} == SimpleBank.withdrawl(bank_pid, "test_id", -1)
     end
 
     test "raises an error if the account does not exist", %{bank: bank_pid}  do
