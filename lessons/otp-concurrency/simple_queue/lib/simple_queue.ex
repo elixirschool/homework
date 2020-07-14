87cb0a358f4b753100705aaad9da5028615bf3fe
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2020-07-14 06:40:54 -0400

    simple queue homework

--- lessons/otp-concurrency/simple_queue/lib/simple_queue.ex
+++ lessons/otp-concurrency/simple_queue/lib/simple_queue.ex
@@ -11,15 +11,18 @@ defmodule SimpleQueue do
 
   def handle_call(:queue, _from, state), do: {:reply, state, state}
 
-  def handle_cast({:enqueue, value}, state) do
-    {:noreply, state ++ [value]}
+  def handle_call({:enqueue, value}, _from, state) do
+    {:reply, value, state ++ [value]}
   end
+
+  def handle_call(:sum, _from, state), do: {:reply, Enum.sum(state), state}
 
   def start_link(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
   def queue, do: GenServer.call(__MODULE__, :queue)
-  def enqueue(value), do: GenServer.cast(__MODULE__, {:enqueue, value})
+  def enqueue(value), do: GenServer.call(__MODULE__, {:enqueue, value})
   def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+  def sum, do: GenServer.call(__MODULE__, :sum)
 end
