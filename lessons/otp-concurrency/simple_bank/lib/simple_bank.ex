795a5d8e7f38626076224a4d017f7ef55bbb5602
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2020-07-14 06:45:01 -0400

    fix simple_bank compilation error

--- lessons/otp-concurrency/simple_bank/lib/simple_bank.ex
+++ lessons/otp-concurrency/simple_bank/lib/simple_bank.ex
@@ -4,15 +4,20 @@ defmodule SimpleBank do
   """
   use GenServer
 
+  @typedoc """
+   An atom describing an error
+   """
+   @type reason :: atom
+
   def start_link(initial_state \\ %{}) do
     GenServer.start_link(__MODULE__, initial_state)
   end
 
-  @spec register(pid(), String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  @spec register(pid(), String.t()) :: {:ok, String.t()} | {:error, reason}
   def register(bank_pid, name) do
   end
 
-  @spec deposit(pid(), String.t(), pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
+  @spec deposit(pid(), String.t(), pos_integer()) :: {:ok, pos_integer()} | {:error, reason}
   def deposit(bank_pid, account_id, amount) when is_integer(amount) and amount > 0 do
   end
 
@@ -20,11 +25,11 @@ defmodule SimpleBank do
     {:error, :missing_account}
   end
 
-  @spec deposit(pid(), String.t()) :: {:ok, pos_integer} | {:error, String.t()}
+  @spec balance(pid(), String.t()) :: {:ok, pos_integer} | {:error, reason}
   def balance(bank_pid, account_id) do
   end
 
-  @spec withdrawl(pid(), String.t(), pos_integer()) :: {:ok, {pos_integer(), pos_integer()}} | {:error, String.t()}
+  @spec withdrawl(pid(), String.t(), pos_integer()) :: {:ok, {pos_integer(), pos_integer()}} | {:error, reason}
   def withdrawl(bank_pid, account_id, amount) do
   end
 
